51c567d3652c6587f60223954c82238e8206f73d
Tetiankaa <tanyakorsun0208@gmail.com>
2024-01-30 14:18:31 +0100

    Initial commit

---
+++ src/main/java/com/example/javaspringhomework/JavaSpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspringhomework/JavaSpringHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
